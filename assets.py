56189db86bfd9e0f0fe92ad2e567bbf3dc2c9a2e
Carl Jason <73461353+Carl-bot-Jason@users.noreply.github.com>
2023-02-10 14:32:32 +0530

    Finished and integrated menu

--- assets.py
+++ assets.py
@@ -1,6 +1,91 @@
 import pygame
+import time
+import math
+import copy
+
+pygame.font.init()
+
+boxList, borderList, XList, OList, menuX, menuO = [], [], [], [], [], []
+turn = 1
+bot = True
+swapTurn = False
+menu = True
+run = True
+pointer = -1
+XFirst = True
+
+# windowWidth, windowHeight = 800, 600
+windowWidth, windowHeight = 1920, 1000
+borderShort = 5
+borderLong = int(.45 * windowHeight) + 2 * borderShort
+
+for i in range(3):
+	boxList.extend([pygame.Rect(
+		int((windowWidth - 3 * 0.15 * windowHeight + 2 * borderShort)/2) + j * (int(0.15 * windowHeight) + borderShort), 
+		int(0.27 * windowHeight + i * (0.15 * windowHeight + borderShort)),
+		int(0.15 * windowHeight),
+		int(0.15 * windowHeight)
+		) for j in range(3)]
+	)
+
+for i in range(2):
+	borderList.append(pygame.Rect(
+		int((windowWidth - 3 * 0.15 * windowHeight + 2 * borderShort)/2 + 0.15 * windowHeight * (i+1) + i * borderShort),
+		int(0.27 * windowHeight),
+		borderShort,
+		borderLong)
+	)
+
+for i in range(2):
+	borderList.append(pygame.Rect(
+		int((windowWidth - 3 * 0.15 * windowHeight + 2 * borderShort)/2),
+		int(0.27 * windowHeight + (i+1) * 0.15 * windowHeight + i * borderShort),
+		borderLong,
+		borderShort)
+	)
+
+menuRect = pygame.Rect(int(windowWidth * 0.80), 0, int(windowWidth * 0.05), int(windowWidth * 0.05))
+count = math.ceil((windowHeight)/(windowWidth * 0.05))
+gap = int(windowWidth * 0.01)
+menuXORect, menuX, menuO = [], [], []
+t = time.time()
+
+for i in range(count):
+	menuRect.top = (i+1) * gap + i * menuRect.width
+	menuXORect.append(copy.copy(menuRect))
 
 c_lightCyan = pygame.Color(107, 192, 209)
 c_brightCyan = pygame.Color(138, 221, 237)
 c_white = pygame.Color(255, 255, 255)
 c_black = pygame.Color(1, 1, 1)
+
+p_menuText_left = int(windowWidth * 0.15)
+p_pointerPvp_left = int(windowWidth * 0.1)
+p_menuTitle_top = int(windowHeight * 0.18)
+p_menuPvp_top = int(windowHeight * 0.45)
+p_menuCpu_top = int(windowHeight * 0.6)
+
+f_InterBold = pygame.font.Font('./assets/Inter-Bold.otf', int(windowWidth * 0.08))
+f_InterLight = pygame.font.Font('./assets/Inter-Light.otf', int(windowWidth * 0.05))
+
+s_title = f_InterBold.render('Tic Tac Toe', True, c_black)
+s_pvp = f_InterLight.render('vs Player', True, c_black)
+s_cpu = f_InterLight.render('vs Computer', True, c_black)
+
+r_title = s_title.get_rect()
+r_title.x = p_menuText_left
+r_title.y = p_menuTitle_top
+r_pvp = s_pvp.get_rect()
+r_pvp.x = p_menuText_left
+r_pvp.y = p_menuPvp_top
+r_cpu = s_cpu.get_rect()
+r_cpu.x = p_menuText_left
+r_cpu.y = p_menuCpu_top
+
+pointerArray = [
+	pygame.Rect(int(windowWidth * 0.10), int(windowHeight * 0.45), int(windowWidth * 0.05), int(windowWidth * 0.05)),
+	pygame.Rect(int(windowWidth * 0.10), int(windowHeight * 0.60), int(windowWidth * 0.05), int(windowWidth * 0.05))
+	]
+
+
+pygame.font.quit()
--- driver.py
+++ driver.py
@@ -1,50 +1,14 @@
 import ttt
 import pygame
 import time
+import copy
 from assets import *
 
 pygame.init()
-windowWidth, windowHeight = 1900, 1000
-borderShort = 5
-borderLong = int(.45 * windowHeight) + 2 * borderShort
 display = pygame.display.set_mode((windowWidth, windowHeight))
 pygame.display.set_caption("Tic Tac Toe")
-boxList = []
-borderList = []
-XList = []
-OList = []
-turn = 1
-bot = True
-swapTurn = False
 
-for i in range(3):
-	boxList.extend([pygame.Rect(
-		int((windowWidth - 3 * 0.15 * windowHeight + 2 * borderShort)/2) + j * (int(0.15 * windowHeight) + borderShort), 
-		int(0.27 * windowHeight + i * (0.15 * windowHeight + borderShort)),
-		int(0.15 * windowHeight),
-		int(0.15 * windowHeight)
-		) for j in range(3)]
-	)
-
-for i in range(2):
-	borderList.append(pygame.Rect(
-		int((windowWidth - 3 * 0.15 * windowHeight + 2 * borderShort)/2 + 0.15 * windowHeight * (i+1) + i * borderShort),
-		int(0.27 * windowHeight),
-		borderShort,
-		borderLong)
-	)
-
-for i in range(2):
-	borderList.append(pygame.Rect(
-		int((windowWidth - 3 * 0.15 * windowHeight + 2 * borderShort)/2),
-		int(0.27 * windowHeight + (i+1) * 0.15 * windowHeight + i * borderShort),
-		borderLong,
-		borderShort)
-	)
-
-run = True
 game = ttt.TTT()
-game.display()
 
 def makeX(rect):
 	len = rect.width
@@ -57,14 +21,51 @@ def makeX(rect):
 def makeO(rect):
 	return (rect.center, int(rect.width * .425))
 
-def drawX(arr):
-	for i in arr:
-		pygame.draw.line(display, c_black, (i[0], i[1]), (i[2], i[3]), 2)
+def drawX(tup, width=5):
+	pygame.draw.line(display, c_black, (tup[0], tup[1]), (tup[2], tup[3]), width)
 
-def drawO(arr):
-	for i in arr:
-		pygame.draw.circle(display, c_black, i[0], i[1], 2)
+def drawO(tup, width=4):
+	pygame.draw.circle(display, c_black, tup[0], tup[1], width)
 
+def makePointer(rect):
+	len = rect.width
+	x1 = rect.x + int(len * 0.25)
+	y1 = rect.y + int(len * 0.45)
+	x2 = rect.x + int(len * 0.65)
+	y2 = rect.y + int(len * 0.70)
+	x3 = x1
+	y3 = rect.y + int(len * 0.95)
+	return [(x1, y1, x2, y2), (x2, y2, x3, y3)]
+
+def drawMenuXO(pixels, XFirst):
+	menuX.clear()
+	menuO.clear()
+	for i in range(count):
+		menuXORect[i].top -= pixels
+		
+	rem_count = 0
+	for i in range(count):
+		if menuXORect[i].top < (-1.5) * menuXORect[i].height:
+			XFirst = not XFirst
+			rem_count += 1
+			continue;
+		break;
+		
+	for i in range(rem_count):
+		menuXORect.pop(0)
+		menuRect.top = menuXORect[-1].top + gap + menuXORect[-1].height
+		menuXORect.append(copy.copy(menuRect))
+
+	X = XFirst
+	for i in range(count):
+		if X:
+			X = not X
+			menuX.extend(makeX(menuXORect[i]))
+			
+		else:
+			X = not X
+			menuO.append(makeO(menuXORect[i]))
+	return XFirst
 
 while run:
 	display.fill((107, 192, 209))
@@ -73,34 +74,64 @@ while run:
 		if event.type == pygame.QUIT:
 			run = False
 		if event.type == pygame.MOUSEBUTTONDOWN:
-			if bot:
-				if turn == 1:
+			if menu:
+				if r_pvp.collidepoint(xy):
+					menu = False
+					bot = False
+				elif r_cpu.collidepoint(xy):
+					menu = False
+					bot = True
+			else:
+				if bot:
+					if turn == 1:
+						for i in range(len(boxList)):
+							if boxList[i].collidepoint(xy):
+								if game.state[i] == '0':
+									game.state = game.place(i, turn)
+									XList.extend(makeX(boxList[i]))
+									swapTurn = True
+				else:
 					for i in range(len(boxList)):
 						if boxList[i].collidepoint(xy):
-							if game.state[i] == '0':
-								game.state = game.place(i, turn)
+							if game.state[i] != '0':
+								break
+							if turn == 1:
+								game.place(i, turn)
 								XList.extend(makeX(boxList[i]))
 								swapTurn = True
-								game.display()
-			else:
-				for i in range(len(boxList)):
-					if boxList[i].collidepoint(xy):
-						if game.state[i] != '0':
-							break
-						if turn == 1:
-							game.place(i, turn)
-							XList.extend(makeX(boxList[i]))
-							swapTurn = True
-						else:
-							game.place(i, turn)
-							OList.append(makeO(boxList[i]))
-							swapTurn = True
-						game.display()
+							else:
+								game.place(i, turn)
+								OList.append(makeO(boxList[i]))
+								swapTurn = True
+	
+	if menu:
+		pointer = -1
+		if r_pvp.collidepoint(xy):
+			pointer = 0
+		elif r_cpu.collidepoint(xy):
+			pointer = 1
+		pixels = 0
+		if time.time() - t > 0.05:
+			t = time.time()
+			pixels = 3
+		display.blit(s_title, (p_menuText_left, p_menuTitle_top))
+		display.blit(s_pvp, (p_menuText_left, p_menuPvp_top))
+		display.blit(s_cpu, (p_menuText_left, p_menuCpu_top))
+		if pointer != -1:
+			for i in makePointer(pointerArray[pointer]):
+				drawX(i, 11)
+
+		XFirst = drawMenuXO(pixels, XFirst)
+		for i in menuX:
+			drawX(i)
+		for i in menuO:
+			drawO(i)
+		pygame.display.flip()
+		continue
 
 	if bot and turn == 2:
-		time.sleep(1)
-		nextState = game.Osearch(game.state, 5)
-		print(nextState)
+		time.sleep(0.5)
+		nextState = game.Osearch(game.state, 4)
 		for i in range(len(nextState)):
 			if nextState[i] != game.state[i]:
 				game.place(i, turn)
@@ -114,8 +145,10 @@ while run:
 	for i in borderList:
 		pygame.draw.rect(display, c_white, i)
 
-	drawX(XList)
-	drawO(OList)
+	for i in XList:
+		drawX(i, 2)
+	for i in OList:
+		drawO(i, 2)
 
 	if swapTurn:
 		turn = 2 if turn == 1 else 1
